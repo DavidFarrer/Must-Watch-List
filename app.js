4a8525841846f6cffee5102af344abae2c9457dc
Dave Farrer <dave.farrer89@gmail.com>
2018-01-25 18:55:17 -0500

    add user capabilities

--- app.js
+++ app.js
@@ -1,11 +1,76 @@
 var express = require("express");
-var app = express();
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mustwatch");
+var db = mongoose.connection;
+
 var indexRoutes = require("./routes/index");
+var userRoutes = require("./routes/users");
 
+// Init app
+var app = express();
 
+// View Engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(indexRoutes);
+// Static Folder
 app.use(express.static(__dirname + "/public"));
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// Express Session
+app.use(session({
+	secret: "topsecret",
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split(".")
+		, root = namespace.shift()
+		, formParam = root;
+
+		while (namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars for Flash
+app.use(function(req, res, next) {
+	res.locals.success_msg = req.flash("succes_msg");
+	res.locals.error_msg = req.flash("error_msg");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
 
 app.listen(3000, () => console.log("Listening on port 3000"));
