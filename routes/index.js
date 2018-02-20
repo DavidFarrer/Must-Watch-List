524ef038ce86153c0e6f9e074f75e57fc3a7bcec
Dave Farrer <dave.farrer89@gmail.com>
2018-02-20 17:54:46 -0500

    drop config file for heroku

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 var imdb = require("imdb-api");
-var config = require("../config");
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 var currentSearch;
